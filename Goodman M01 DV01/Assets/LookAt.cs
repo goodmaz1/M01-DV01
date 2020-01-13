7f87cfae77d99d91569872e47d2fdb5281393261
Zack Goodman <zgoodman.student@gmail.com>
2020-01-13 15:44:51 -0700

    Added Level
    Ghosts work (Look At/Away, Move Toward/Away)
    Commented Code
    No UI

---
+++ Goodman M01 DV01/Assets/LookAt.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform player;
+    public bool moving = false;
+    public float speed = 10f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player);
+
+        if (moving == true)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Goodman M01 DV01/Assets/LookAway.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class LookAway : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 away;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 away = transform.position - player.transform.position;
+        transform.rotation = Quaternion.LookRotation(away);
+    }
+}
